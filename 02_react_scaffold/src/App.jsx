cfbd030d464a93f4d6a52873e536a5229d990ada
debug-10 <37249116@qq.com>
2025-02-17 16:34:54 +0800

    react基础学习

---
+++ 02_react_scaffold/src/App.jsx
@@ -0,0 +1,13 @@
+// import HelloWorld from "./components/HelloWorld";
+import TaskList from "./components/TaskList";
+
+function App() {
+  return (
+    <div>
+      {/* <HelloWorld /> */}
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
